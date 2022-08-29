ad0a15e6eda4fb7ba8b3f77c93b11033c42fdbc8
Storm OBryant <vmarie1997@gmail.com>
2022-08-29 15:02:13 -0500

    added 7 day forecast

--- forecast.js
+++ forecast.js
@@ -1,16 +1,29 @@
 const axios = require('axios')
 
+// const getWeather = (lat, lon) => {
+// 	const API = `http://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&units=I`
+// 	const weatherInfo = axios.get(API).catch((err) => console.log('Error: something went wrong from getWeather at server', err))
+// 	return weatherInfo
+// }
+// i wanna get a 7 day forecast
 const getWeather = (lat, lon) => {
-	const API = `http://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&units=I`
+	const API = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=7&units=I&key=${process.env.WEATHER_API_KEY}`
 	const weatherInfo = axios.get(API).catch((err) => console.log('Error: something went wrong from getWeather at server', err))
 	return weatherInfo
 }
 
 class Forecast {
 	constructor(chosenCity) {
-		this.date = chosenCity.ob_time;
-		this.description = `The temperature is ${chosenCity.temp}, but feels like ${chosenCity.app_temp} with ${chosenCity.weather.description.toLowerCase()}`;
+		this.date = chosenCity.valid_date;
+		this.description = `The temperature right now is ${chosenCity.temp} with a high of ${chosenCity.high_temp} and a low of ${chosenCity.low_temp}, but could feel anywhere between ${chosenCity.app_min_temp} and ${chosenCity.app_max_temp} with ${chosenCity.weather.description.toLowerCase()}`;
 	}
 }
 
-module.exports = { Forecast, getWeather};
+const pushForecast = (chosenCity) => {
+	const forecastArr = []
+	chosenCity.map(day => forecastArr.push(new Forecast(day)))
+
+	return forecastArr
+}
+
+module.exports = { Forecast, getWeather, pushForecast};
--- routes/weatherRoute.js
+++ routes/weatherRoute.js
@@ -4,13 +4,14 @@ const weather = require('../forecast')
 
 router.get('/weather', async (req, res) => {
 	const getWeather = weather.getWeather
-	const Forecast = weather.Forecast
+	const pushForecast = weather.pushForecast
+	// const Forecast = weather.Forecast
 	//the url will look like: /weather/?lat=83&lon=7
 	try {
 		const { lat, lon } = req.query
 		const chosenCity = await getWeather(lat, lon)
-		const forecast = new Forecast(chosenCity.data.data[0])
-		res.send(forecast)
+		const results = pushForecast(chosenCity.data.data)
+		res.send(results)
 	} catch (error) {
 		res.status(500).send(error)
 	}
